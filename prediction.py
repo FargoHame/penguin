8ca9d288821db30eb1bb2fd32a2ffba1d52918de
Skillocity <79521936+Skillocity@users.noreply.github.com>
2021-05-17 23:40:16 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -50,7 +50,8 @@ df = df[:1] # Selects only the first row (the user input data)
 # Displays the user input features
 st.subheader('User Input features')
 
-
+st.subheader('Class labels and their corresponding index number')
+st.write(penguins_species)
 
 # Reads in saved classification model
 load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
