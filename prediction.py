0c880bf5760b2fbef68eec02248d0b7c4cadccc8
Skillocity <79521936+Skillocity@users.noreply.github.com>
2021-05-17 23:24:32 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -13,26 +13,8 @@ Note: User inputs for features are taken from the sidebar. It is located at the 
 
 st.sidebar.header('User Input Features')
 
-
-
-
 def user_input_features():
-    island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-    sex = st.sidebar.selectbox('Sex',('male','female'))
-        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
-        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        data = {'island': island,
-                'bill_length_mm': bill_length_mm,
-                'bill_depth_mm': bill_depth_mm,
-                'flipper_length_mm': flipper_length_mm,
-                'body_mass_g': body_mass_g,
-                'sex': sex}
-        features = pd.DataFrame(data, index=[0])
-        return features
-    input_df = user_input_features()
-                        
+        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
         sex = st.sidebar.selectbox('Sex',('male','female'))
         bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
         bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
@@ -47,6 +29,7 @@ def user_input_features():
         features = pd.DataFrame(data, index=[0])
         return features
     input_df = user_input_features()
+    
 
 # Combines user input features with entire penguins dataset
 # This will be useful for the encoding phase
@@ -66,7 +49,11 @@ df = df[:1] # Selects only the first row (the user input data)
 # Displays the user input features
 st.subheader('User Input features')
 
-
+if uploaded_file is not None:
+    st.write(df)
+else:
+    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
+    st.write(df)
 
 # Reads in saved classification model
 load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
