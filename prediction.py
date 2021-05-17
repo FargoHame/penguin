3a8d86c54c379a50540b8a9400f9d85c46d9760b
Skillocity <79521936+Skillocity@users.noreply.github.com>
2021-05-17 23:20:04 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -6,23 +6,33 @@ from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
 # Penguin Prediction App
-This app predicts the **Palmer Penguin** species!
-Data obtained from the [palmerpenguins library](https://github.com/allisonhorst/palmerpenguins) in R by Allison Horst.
+This app predicts the species of Palmer penguins found in Antarctica using Machine Learning!
+Dataset credits: Dr.Kristen Gorman and Palmer Station, Antarctica LTER and Allison Horst.
+Note: User inputs for features are taken from the sidebar. It is located at the top left of the page (arrow symbol). The values of parameters can be changed from the sidebar.
 """)
 
 st.sidebar.header('User Input Features')
 
-st.sidebar.markdown("""
-[Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
-""")
 
-# Collects user input features into dataframe
-uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-if uploaded_file is not None:
-    input_df = pd.read_csv(uploaded_file)
-else:
-    def user_input_features():
-        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
+
+
+def user_input_features():
+    island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
+    sex = st.sidebar.selectbox('Sex',('male','female'))
+        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
+        bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
+        flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
+        body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
+        data = {'island': island,
+                'bill_length_mm': bill_length_mm,
+                'bill_depth_mm': bill_depth_mm,
+                'flipper_length_mm': flipper_length_mm,
+                'body_mass_g': body_mass_g,
+                'sex': sex}
+        features = pd.DataFrame(data, index=[0])
+        return features
+    input_df = user_input_features()
+                        
         sex = st.sidebar.selectbox('Sex',('male','female'))
         bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
         bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
@@ -56,11 +66,7 @@ df = df[:1] # Selects only the first row (the user input data)
 # Displays the user input features
 st.subheader('User Input features')
 
-if uploaded_file is not None:
-    st.write(df)
-else:
-    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-    st.write(df)
+
 
 # Reads in saved classification model
 load_clf = pickle.load(open('penguins_clf.pkl', 'rb'))
